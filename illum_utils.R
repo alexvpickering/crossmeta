c2979dae3623e3e08a3a24158b10a20ccff007df
Alex Pickering <alexvpickering@gmail.com>
2016-02-19 23:34:47 -0800

    add surrogate variable analysis

--- illum_utils.R
+++ illum_utils.R
@@ -4,27 +4,28 @@ library(GEOquery)
 library(stringr)
 library(tcltk)
 library(RColorBrewer)
+library(sva)
 library(limma)
 
 #------------------------
 
 inputs <- function(instructions="", two=F, box1="AL group name", box2="CR group name"){
-  #IN:
-  #OUT:
-  
-  xvar <- tclVar("")
-  if (two) {
+    #IN:
+    #OUT:
+
+    xvar <- tclVar("")
+    if (two) {
     yvar <- tclVar("")
-  }
-  
-  tt <- tktoplevel()
-  tkwm.title(tt,"")
-  x.entry <- tkentry(tt, textvariable=xvar)
-  if (two) {
+    }
+
+    tt <- tktoplevel()
+    tkwm.title(tt,"")
+    x.entry <- tkentry(tt, textvariable=xvar)
+    if (two) {
     y.entry <- tkentry(tt, textvariable=yvar)
-  }
-  
-  submit <- function() {
+    }
+
+    submit <- function() {
     e <- parent.env(environment())   
     x <- as.character(tclvalue(xvar))
     e$x <- x
@@ -33,37 +34,37 @@ inputs <- function(instructions="", two=F, box1="AL group name", box2="CR group 
       e$y <- y
     }
     tkdestroy(tt)
-  }
-  submit.but <- tkbutton(tt, text="Submit", command=submit)
-  
-  reset <- function() {
+    }
+    submit.but <- tkbutton(tt, text="Submit", command=submit)
+
+    reset <- function() {
     tclvalue(xvar)<-""
     if (two) {
       tclvalue(yvar)<-""
     }
-  }
-  reset.but <- tkbutton(tt, text="Reset", command=reset)
-  
-  tkgrid(tklabel(tt,text=instructions),columnspan=2)
-  tkgrid(tklabel(tt,text=box1), x.entry, pady = 10, padx =10)
-  if (two) {
+    }
+    reset.but <- tkbutton(tt, text="Reset", command=reset)
+
+    tkgrid(tklabel(tt,text=instructions),columnspan=2)
+    tkgrid(tklabel(tt,text=box1), x.entry, pady = 10, padx =10)
+    if (two) {
     tkgrid(tklabel(tt,text=box2), y.entry, pady = 10, padx =10)
-  }
-  tkgrid(submit.but, reset.but)
-  
-  tkwait.window(tt)
-  if (two) {
+    }
+    tkgrid(submit.but, reset.but)
+
+    tkwait.window(tt)
+    if (two) {
     return (c(x,y))
-  } else {
+    } else {
     return (x)
-  }
+    }
 }
 
 #------------------------
 
 get_raw <- function(gse_name, data_dir) {
-  #wrapper for get_raw_one
-  lapply(gse_name, get_raw_one, data_dir)
+    #wrapper for get_raw_one
+    lapply(gse_name, get_raw_one, data_dir)
 }
 
 
@@ -109,10 +110,10 @@ open_raw <- function (gse_names, data_dir) {
 #------------------------
 
 load_eset <- function (gse_name, data_dir) {
-  #wrapper for load_eset_one
-  esets <- lapply(gse_name, load_eset_one, data_dir)
-  names(eset) <- gse_name
-  return (eset)
+    #wrapper for load_eset_one
+    esets <- lapply(gse_name, load_eset_one, data_dir)
+    names(eset) <- gse_name
+    return (eset)
 }
 
 
@@ -145,118 +146,122 @@ load_eset_one <- function (gse_name, data_dir) {
 #------------------------
 
 diff_expr <- function (eset) {
-  #wrapper for diff_expr_one
-  top_tables <- mapply(diff_expr_one, eset, names(eset))
-  names(top_tables) <- names(eset)
-  return (top_tables)
+    #wrapper for diff_expr_one
+    top_tables <- mapply(diff_expr_one, eset, names(eset))
+    names(top_tables) <- names(eset)
+    return (top_tables)
 }
 
 
 diff_expr_one <- function (eset, name) {
-  #INPUT:
-  #OUTPUT:
-  
-  #BLOCKING VARIABLES:
-  #-------------------
-  block_names <- c()
-  
-  #ask if want to add blocking variable?
-  choices <- paste(sampleNames(eset), pData(eset)$title)
-  i <- 1  #block count
-  while (TRUE){
-    block <- tk_select.list(c(choices, "YES", "NO"), title="Add blocking variable?")
-    if (block != "YES") {break}
-    
-    #if YES, setup
-    block_name <- paste("block", i, sep="_")
-    block_names <- c(block_names, block_name)
-    pData(eset)[, block_name] <- "level_0"  #baseline level
-    i <- i + 1
-    
-    #select samples in each level of blocking variable (except 1)
-    j <- 1  #level count 
+    #INPUT:
+    #OUTPUT:
+
+    #BLOCKING VARIABLES:
+    #-------------------
+    block_names <- c()
+
+    #ask if want to add blocking variable?
+    choices <- paste(sampleNames(eset), pData(eset)$title)
+    i <- 1  #block count
+    while (TRUE){
+        block <- tk_select.list(c(choices, "YES", "NO"), title="Add blocking variable?")
+        if (block != "YES") {break}
+
+        #if YES, setup
+        block_name <- paste("block", i, sep="_")
+        block_names <- c(block_names, block_name)
+        pData(eset)[, block_name] <- "level_0"  #baseline level
+        i <- i + 1
+
+        #select samples in each level of blocking variable (except 1)
+        j <- 1  #level count 
     while (TRUE) {
       level <- tk_select.list(choices, multiple=T, title="Select samples in each level (except 1)")    
       level <- str_extract(level, "GSM[0-9]+")
       if (length(level) == 0) {break}  
-      
+
       #add level to pheno
       level_name <- paste("level", j, sep="_")
       pData(eset)[level, block_name] <- level_name
       j <- j + 1
     }
-  }
+}
   
   
-  #CONTRASTS:        
-  #----------
+    #CONTRASTS:        
+    #----------
+
+    choices <- paste(sampleNames(eset), pData(eset)$title)
+    contrasts <- c()
+    group_levels <- c()
+    selected_samples <- c()
+
+    #repeat until all contrasts selected
+    while (TRUE) {
+        #select AL samples
+        AL <- tk_select.list(choices, multiple=T, title="select AL (control) samples for contrast")
+        AL <- str_extract(AL, "GSM[0-9]+")
+        if (length(AL) == 0) {break}
+
+        #select CR samples
+        CR <- tk_select.list(choices, multiple=T, title="select CR (test) samples for contrast")
+        CR <- str_extract(CR, "GSM[0-9]+")
+
+        #add group names to pheno
+        group_names <- inputs("Enter group names", two=T)
+        pData(eset)[AL, "group"] <- group_names[1]
+        pData(eset)[CR, "group"] <- group_names[2]
+
+        #add to contrasts
+        contrasts <- c(contrasts, paste(group_names[2], group_names[1], sep="-"))
+
+        #add to group_levels
+        group_levels <- unique(c(group_levels, group_names[1], group_names[2]))
+
+        #add to selected_samples
+        selected_samples <- unique(c(selected_samples, AL, CR))
+    }
+    #retain selected samples only
+    eset <- eset[, selected_samples]
   
-  choices <- paste(sampleNames(eset), pData(eset)$title)
-  contrasts <- c()
-  group_levels <- c()
-  selected_samples <- c()
-  
-  #repeat until all contrasts selected
-  while (TRUE) {
-    #select AL samples
-    AL <- tk_select.list(choices, multiple=T, title="select AL (control) samples for contrast")
-    AL <- str_extract(AL, "GSM[0-9]+")
-    if (length(AL) == 0) {break}
+    #MDS PLOT:
+    #---------
+
+    group <- factor(pData(eset)$group, levels=group_levels)
+    palette <- brewer.pal(8, "Dark2")
+    colours <- palette[group]
+
+    plotMDS(exprs(eset), labels = sampleNames(eset), 
+            main = name, col = colours)
+
+
+    #DIFFERENTIAL EXPRESSION (limma):
+    #-------------------------------
+
+    #make full model matrix   
+    vars <- c("~0", "group", block_names)
+    fmla <- as.formula(paste(vars, collapse="+"))
+    mod <- model.matrix(fmla, data=pData(eset))
+    colnames(mod)[seq_along(group_levels)] <- group_levels
+
+    #make null model matrix (sva)
+    vars0 <- c("~1", block_names)
+    fmla0 <- as.formula(paste(vars0, collapse="+"))
+    mod0 <- model.matrix(fmla0, data=pData(eset))
+
+    svobj <- sva(exprs(eset), mod, mod0)
+    modSv <- cbind(mod, SV1=svobj$sv)  #TODO: if #SV > 1
+    contrast_matrix <- makeContrasts(contrasts=contrasts, levels=modSv)
+    print (modSv)
     
-    #select CR samples
-    CR <- tk_select.list(choices, multiple=T, title="select CR (test) samples for contrast")
-    CR <- str_extract(CR, "GSM[0-9]+")
-    
-    #add group names to pheno
-    group_names <- inputs("Enter group names", two=T)
-    pData(eset)[AL, "group"] <- group_names[1]
-    pData(eset)[CR, "group"] <- group_names[2]
-    
-    #add to contrasts
-    contrasts <- c(contrasts, paste(group_names[2], group_names[1], sep="-"))
-    
-    #add to group_levels
-    group_levels <- unique(c(group_levels, group_names[1], group_names[2]))
-    
-    #add to selected_samples
-    selected_samples <- unique(c(selected_samples, AL, CR))
-  }
-  #retain selected samples only
-  eset <- eset[, selected_samples]
-  
-  #make model/contrast matrix   
-  variables <- c("~0", "group", block_names)
-  formula <- as.formula(paste(variables, collapse="+"))
-  model <- model.matrix(formula, data=pData(eset))
-  colnames(model)[seq_along(group_levels)] <- group_levels
-  contrast_matrix <- makeContrasts(contrasts=contrasts, levels=model)
-  
-  print (model)
-  print (contrast_matrix)
-  
-  
-  #MDS PLOT:
-  #---------
-  
-  group <- factor(pData(eset)$group, levels=group_levels)
-  palette <- brewer.pal(8, "Dark2")
-  colours <- palette[group]
-    
-  plotMDS(exprs(eset), labels = group, 
-          main = name, col = colours)
-          
-  
-  
-  #DIFFERENTIAL EXPRESSION (limma):
-  #-------------------------------
-  
-  fit <- contrasts.fit (lmFit(exprs(eset), model), contrast_matrix)
-  ebayes <- eBayes(fit)
-  
-  top_tables <- list()
-  for (i in seq_along(contrasts)){
-    top_genes <- topTable(ebayes, coef=i, n=Inf, resort.by="logFC", p.value=0.05, lfc=1)
-    top_tables[[contrasts[i]]] <- top_genes
-  }
-  return (top_tables)
+    fit <- contrasts.fit (lmFit(exprs(eset),modSv), contrast_matrix)
+    ebayes <- eBayes(fit)
+
+    top_tables <- list()
+    for (i in seq_along(contrasts)){
+        top_genes <- topTable(ebayes, coef=i, n=Inf, resort.by="logFC", p.value=0.05, lfc=1)
+        top_tables[[contrasts[i]]] <- top_genes
+    }
+    return (top_tables)
 }
