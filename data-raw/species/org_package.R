0e40045097664bfba2a058fc4b715b885b738daf
alexvpickering <alexvpickering@gmail.com>
2017-08-08 18:41:12 -0700

    org_pkg raw-data
    Former-commit-id: 88cf937321751cd20f7d600b2648b594aa091f64

---
+++ data-raw/species/org_package.R
@@ -0,0 +1,23 @@
+# package names from https://bioconductor.org/packages/3.5/data/annotation/
+
+org_pkg <- fread("PASTE TEXT HERE", header = FALSE)
+org_pkg <- org_pkg$V1[org_pkg$V2 == 'Bioconductor Package Maintainer']
+
+# get packages
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite(org_pkg)
+
+package_organisms <- c()
+
+for (package_name in org_pkg) {
+
+    pkg <- crossmeta:::get_biocpack(package_name)
+    package_organisms <- c(package_organisms, AnnotationDbi::species(pkg))
+}
+
+names(org_pkg) <- package_organisms
+
+devtools::use_data(gpl_bioc, homologene, sources, token, org_pkg, internal = TRUE, overwrite = TRUE)
+
+
