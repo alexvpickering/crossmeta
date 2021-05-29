e59888a33b8e1058486bd7853cd3c679d18f4a5c
Alex Pickering <alexvpickering@gmail.com>
2021-05-29 07:35:51 -0700

    remove DESeq2 dep

--- R/filter_genes.R
+++ R/filter_genes.R
@@ -13,10 +13,9 @@
 #' @examples
 #'
 #' # example ExpressionSet
-#' dds <- DESeq2::makeExampleDESeqDataSet()
-#' eset <- Biobase::ExpressionSet(DESeq2::counts(dds))
-#' eset$group <- dds$condition
+#' eset <- makeExampleCountsEset()
 #' eset <- filter_genes(eset)
+#' 
 filter_genes <- function(eset) {
   els <- Biobase::assayDataElementNames(eset)
   els <- ifelse("counts" %in% els, "counts", "exprs")
@@ -26,4 +25,33 @@ filter_genes <- function(eset) {
   eset <- eset[keep, ]
   if (!nrow(eset)) stop("No genes with reads after filtering")
   return(eset)
+}
+
+# adapted from DESeq2::makeExampleDESeqDataSet
+makeExampleCountsEset <- function (n = 1000, m = 12, betaSD = 0, interceptMean = 4, interceptSD = 2, dispMeanRel = function(x) 4/x + 0.1, sizeFactors = rep(1, m)) {
+  beta <- cbind(rnorm(n, interceptMean, interceptSD), rnorm(n, 0, betaSD))
+  dispersion <- dispMeanRel(2^(beta[, 1]))
+  colData <- data.frame(condition = factor(rep(c("A", "B"), 
+                                              times = c(ceiling(m/2), floor(m/2)))))
+  x <- if (m > 1) {
+    stats::model.matrix.default(~colData$condition)
+  }
+  else {
+    cbind(rep(1, m), rep(0, m))
+  }
+  mu <- t(2^(x %*% t(beta)) * sizeFactors)
+  countData <- matrix(rnbinom(m * n, mu = mu, size = 1/dispersion), 
+                      ncol = m)
+  
+  colnames(countData) <- paste("sample", 1:m, sep = "")
+  row.names(countData) <- paste0("gene", 1:n)
+  
+  eset <- Biobase::ExpressionSet(countData)
+  eset$group <- colData$condition
+  return(eset)
+
+}
+
+dispMeanRel <- function(x) {
+  4/x + 0.1
 }
--- R/get_vsd.R
+++ R/get_vsd.R
@@ -1,41 +1,20 @@
 #' Get variance stabilized data for exploratory data analysis
 #'
 #' @param eset ExpressionSet loaded with \link{load_raw}.
-#'   Requires group column in \code{pData(eset)} specifying sample groupings.
-#' @param rlog_cutoff Sample number above which will use
-#'   \code{\link[DESeq2]{vst}} instead of \code{\link[DESeq2]{rlog}}.
-#'   Default is 50.
 #'
-#' @return \code{DESeqTransform} with variance stabilized expression data.
+#' @return \code{matrix} with variance stabilized expression data.
 #' 
 #' @export
 #' 
-get_vsd <- function(eset, rlog_cutoff = 50) {
-  trans_fun <- if (ncol(eset) > rlog_cutoff) DESeq2::vst else DESeq2::rlog
-  els <- Biobase::assayDataElementNames(eset)
+get_vsd <- function(eset) {
   pdata <- Biobase::pData(eset)
+  lib.size <- pdata$lib.size * pdata$norm.factors
   
-  if (all(c("abundance", "counts", "length") %in% els)) {
-    txi.deseq <- list(
-      countsFromAbundance = "no",
-      abundance = Biobase::assayDataElement(eset, "abundance"),
-      counts = Biobase::assayDataElement(eset, "counts"),
-      length = Biobase::assayDataElement(eset, "length")
-    )
-    
-    dds <- DESeq2::DESeqDataSetFromTximport(txi.deseq,
-                                            pdata,
-                                            design = ~group
-    )
-  } else {
-    # this is e.g. for eset from load_archs4_seq
-    dds <- DESeq2::DESeqDataSetFromMatrix(Biobase::exprs(eset),
-                                          pdata,
-                                          design = ~group
-    )
-  }
-  dds <- DESeq2::estimateSizeFactors(dds)
-  vsd <- trans_fun(dds, blind = FALSE)
+  # GK Smyth advice for variance stabilization for plotting
+  # https://support.bioconductor.org/p/114133/
+  vsd <- edgeR::cpm(Biobase::exprs(eset),
+                    lib.size = lib.size,
+                    log = TRUE, prior.count = 5)
   
   return(vsd)
 }
