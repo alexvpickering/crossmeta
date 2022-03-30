b99d7564323d7074f76c94ee0542f95d2c807bf1
Alex Pickering <alexvpickering@gmail.com>
2022-03-29 21:21:24 -0700

    handle old diff_exprs; reindent; closes #12

--- R/meta_utils.R
+++ R/meta_utils.R
@@ -49,126 +49,153 @@
 #'
 #' # perform meta-analysis
 #' es <- es_meta(anals, by_source = TRUE)
-
+#' 
 es_meta <- function(diff_exprs, cutoff = 0.3, by_source = FALSE) {
-
-    # used for analysis per diff_exprs
-    es_meta_src <- function(diff_exprs, cutoff) {
-
-        # get dp and vardp
-        es <- get_es(diff_exprs, cutoff)
-
-        # if just one contrast, use its values
-        if (ncol(es$filt) == 2) {
-            es$filt$mu  <- es$filt[[1]]
-            es$filt$var <- es$filt[[2]]
-            es$filt$fdr <- diff_exprs[[1]]$top_tables[[1]]$adj.P.Val
-
-            return(es)
-        }
-        
-        # only proceed with genes present in 2+ studies
-        df <- es$filt
-        row_nas <- rowSums(is.na(df))
-        can_ma  <- row_nas < ncol(df) / 2
-        
-        df <- df[can_ma, ]
-        
-        # can't have negative variances
-        # TODO: investigate how these arise
-        var_cols <- seq(2, ncol(df), 2)
-        neg_var <- apply(df[, var_cols], 1, function(row) any(row < 0, na.rm = TRUE))
-        df <- df[!neg_var, ]
-        
-        dp  <- df[, seq(1, ncol(df), 2)]
-        var <- df[, seq(2, ncol(df), 2)]
-
-        # get Cochran Q statistic
-        Q <- f.Q(dp, var)
-
-        # get tau (between study variance)
-        tau <- tau2.DL(Q,
-                       num.studies = apply(var, 1, function(x) sum(!is.na(x))),
-                       my.weights  = 1 / var)
-
-        # add tau to vardp then calculate mean effect sizes and variance
-        var <- var + tau
-        df$mu  <- mu.tau2(dp, var)
-        df$var <- var.tau2(var)
-
-        # get z-score and fdr
-        df$z    <- df$mu/sqrt(df$var)
-        fdr     <- fdrtool::fdrtool(df$z, plot = FALSE, verbose = FALSE)
-        df$pval <- fdr$pval
-        df$fdr  <- fdr$qval
-
-        es$filt <- df[order(df$fdr), ]
-
-        return(es)
+  
+  # used for analysis per diff_exprs
+  es_meta_src <- function(diff_exprs, cutoff) {
+    
+    # get dp and vardp
+    diff_exprs <- patch_diff_exprs(diff_exprs)
+    es <- get_es(diff_exprs, cutoff)
+    
+    # if just one contrast, use its values
+    if (ncol(es$filt) == 2) {
+      es$filt$mu  <- es$filt[[1]]
+      es$filt$var <- es$filt[[2]]
+      es$filt$fdr <- diff_exprs[[1]]$top_tables[[1]]$adj.P.Val
+      
+      return(es)
     }
-
-
-    if (by_source) {
-        # check for sources
-        null_sources <- sapply(diff_exprs, function(anal) is.null(anal$sources))
-
-        if (any(null_sources)) {
-            message("Sources missing from diff_exprs (to add, use add_sources).\nContinuing with by_source = FALSE.")
-            es <- list(all = es_meta_src(diff_exprs, cutoff))
-
-        } else {
-            anals_src <- list(all = diff_exprs)
-            anals_src <- c(anals_src, setup_src(diff_exprs, "top_tables"))
-
-            es <- lapply(anals_src, es_meta_src, cutoff)
-        }
-
-
-    } else {
-        es <- list(all = es_meta_src(diff_exprs, cutoff))
-    }
-
+    
+    # only proceed with genes present in 2+ studies
+    df <- es$filt
+    row_nas <- rowSums(is.na(df))
+    can_ma  <- row_nas < ncol(df) / 2
+    
+    df <- df[can_ma, ]
+    
+    # can't have negative variances
+    # TODO: investigate how these arise
+    var_cols <- seq(2, ncol(df), 2)
+    neg_var <- apply(df[, var_cols], 1, function(row) any(row < 0, na.rm = TRUE))
+    df <- df[!neg_var, ]
+    
+    dp  <- df[, seq(1, ncol(df), 2)]
+    var <- df[, seq(2, ncol(df), 2)]
+    
+    # get Cochran Q statistic
+    Q <- f.Q(dp, var)
+    
+    # get tau (between study variance)
+    tau <- tau2.DL(Q,
+                   num.studies = apply(var, 1, function(x) sum(!is.na(x))),
+                   my.weights  = 1 / var)
+    
+    # add tau to vardp then calculate mean effect sizes and variance
+    var <- var + tau
+    df$mu  <- mu.tau2(dp, var)
+    df$var <- var.tau2(var)
+    
+    # get z-score and fdr
+    df$z    <- df$mu/sqrt(df$var)
+    fdr     <- fdrtool::fdrtool(df$z, plot = FALSE, verbose = FALSE)
+    df$pval <- fdr$pval
+    df$fdr  <- fdr$qval
+    
+    es$filt <- df[order(df$fdr), ]
+    
     return(es)
+  }
+  
+  
+  if (by_source) {
+    # check for sources
+    null_sources <- sapply(diff_exprs, function(anal) is.null(anal$sources))
+    
+    if (any(null_sources)) {
+      message("Sources missing from diff_exprs (to add, use add_sources).\nContinuing with by_source = FALSE.")
+      es <- list(all = es_meta_src(diff_exprs, cutoff))
+      
+    } else {
+      anals_src <- list(all = diff_exprs)
+      anals_src <- c(anals_src, setup_src(diff_exprs, "top_tables"))
+      
+      es <- lapply(anals_src, es_meta_src, cutoff)
+    }
+    
+    
+  } else {
+    es <- list(all = es_meta_src(diff_exprs, cutoff))
+  }
+  
+  return(es)
+}
+
+# provides backwards compatibility for older versions of diff_exprs
+# such as found in package lydata
+patch_diff_exprs <- function(diff_exprs) {
+  
+  for (i in seq_along(diff_exprs)) {
+    diff_expr <- diff_exprs[[i]]
+    gse_name <- names(diff_exprs)[i]
+    
+    tts <- diff_expr$top_tables
+    for (j in seq_along(tts)) {
+      tt <- tts[[j]]
+      if (all(c("dprime", "vardprime") %in% colnames(tt))) next()
+      
+      gse_con <- names(tts)[j]
+      con <- gsub(paste0('^', gse_name, '_'), '', gse_con)
+      groups <- strsplit(con, '-', fixed = TRUE)[[1]]
+      
+      tts[[gse_con]] <- add_es(tt, diff_expr$ebayes_sv, groups = groups)
+    }
+    
+    diff_exprs[[gse_name]]$top_tables <- tts
+  }
+  
+  return(diff_exprs)
 }
 
 
 # used by es_meta and path_meta to group top/padog tables by source
 setup_src <- function(anals, ttype = c("top_tables", "padog_tables")) {
-
-    anals_srcs <- list()
-
-    # contrast sources
-    con_src <- unlist(sapply(unname(anals), `[[`, 'sources'))
-
-    # get added pairs
-    added_prs <- lapply(anals, function(anal) anal$pair)
-    added_prs <- unique(unlist(added_prs, recursive = FALSE, use.names = FALSE))
-
-    # get non-pair sources
-    is_prs <- con_src %in% unlist(added_prs)
-    added_src <- sapply(unique(con_src[!is_prs]), list, USE.NAMES = FALSE)
-
-
-    # get anals by source
-    for (src in c(added_prs, added_src)) {
-
-        src_name <- paste(src, collapse = ', ')
-
-        anals_srcs[[src_name]] <- lapply(anals, function(anal) {
-            is_src <- con_src[names(anal[[ttype]])] %in% src
-
-            if (any(is_src)) {
-                anal[[ttype]] <- anal[[ttype]][is_src]
-                anal
-            }
-            else NULL
-        })
-    }
-
-    # remove NULL entries
-    anals_srcs <- lapply(anals_srcs, function(anals_src) anals_src[!sapply(anals_src, is.null)])
-
-    return(anals_srcs)
+  
+  anals_srcs <- list()
+  
+  # contrast sources
+  con_src <- unlist(sapply(unname(anals), `[[`, 'sources'))
+  
+  # get added pairs
+  added_prs <- lapply(anals, function(anal) anal$pair)
+  added_prs <- unique(unlist(added_prs, recursive = FALSE, use.names = FALSE))
+  
+  # get non-pair sources
+  is_prs <- con_src %in% unlist(added_prs)
+  added_src <- sapply(unique(con_src[!is_prs]), list, USE.NAMES = FALSE)
+  
+  
+  # get anals by source
+  for (src in c(added_prs, added_src)) {
+    
+    src_name <- paste(src, collapse = ', ')
+    
+    anals_srcs[[src_name]] <- lapply(anals, function(anal) {
+      is_src <- con_src[names(anal[[ttype]])] %in% src
+      
+      if (any(is_src)) {
+        anal[[ttype]] <- anal[[ttype]][is_src]
+        anal
+      }
+      else NULL
+    })
+  }
+  
+  # remove NULL entries
+  anals_srcs <- lapply(anals_srcs, function(anals_src) anals_src[!sapply(anals_src, is.null)])
+  
+  return(anals_srcs)
 }
 
 
@@ -180,28 +207,28 @@ setup_src <- function(anals, ttype = c("top_tables", "padog_tables")) {
 # @return data.frame with dprime and vardprime values.
 
 get_es <- function(diff_exprs, cutoff = 0.3) {
-
-    # get top tables
-    es <- lapply(diff_exprs, function(study) study$top_tables)
-    nm <- unlist(lapply(es, names))
-    nm <- paste(c('dprime', 'vardprime'), rep(nm, each=2), sep='.')
-    es <- unlist(es, recursive = FALSE, use.names = FALSE)
-
-    # get desired top table columns
-    es <- lapply(es, function(top) {
-        top$SYMBOL <- row.names(top)
-        top[, c("SYMBOL", "dprime", "vardprime")]
-    })
-
-    # merge dataframes
-    es <- merge_dataframes(es)
-    names(es) <- nm
-
-
-    # only keep genes where more than cutoff fraction of studies have data
-    filt <- apply(es, 1, function(x) sum(!is.na(x))) >= (ncol(es) * cutoff)
-
-    return(list(filt = es[filt, ], raw = es))
+  
+  # get top tables
+  es <- lapply(diff_exprs, function(study) study$top_tables)
+  nm <- unlist(lapply(es, names))
+  nm <- paste(c('dprime', 'vardprime'), rep(nm, each=2), sep='.')
+  es <- unlist(es, recursive = FALSE, use.names = FALSE)
+  
+  # get desired top table columns
+  es <- lapply(es, function(top) {
+    top$SYMBOL <- row.names(top)
+    top[, c("SYMBOL", "dprime", "vardprime")]
+  })
+  
+  # merge dataframes
+  es <- merge_dataframes(es)
+  names(es) <- nm
+  
+  
+  # only keep genes where more than cutoff fraction of studies have data
+  filt <- apply(es, 1, function(x) sum(!is.na(x))) >= (ncol(es) * cutoff)
+  
+  return(list(filt = es[filt, ], raw = es))
 }
 
 
@@ -215,21 +242,21 @@ get_es <- function(diff_exprs, cutoff = 0.3) {
 
 
 merge_dataframes <- function(ls, keys = "SYMBOL") {
-
-    # ensure non 'by' names are not duplicated
-    ls = Map(function(x, i)
-        stats::setNames(x, ifelse(names(x) %in% keys,
-                                  names(x),
-                                  sprintf('%s.%d', names(x), i))),
-        ls, seq_along(ls))
-
-    # merge list
-    res <- Reduce(function(...) merge(..., by = keys, all = TRUE), ls)
-
-    # format result
-    row.names(res) <- res[, keys[1]]
-    res[, keys[1]] <- NULL
-    return(res)
+  
+  # ensure non 'by' names are not duplicated
+  ls = Map(function(x, i)
+    stats::setNames(x, ifelse(names(x) %in% keys,
+                              names(x),
+                              sprintf('%s.%d', names(x), i))),
+    ls, seq_along(ls))
+  
+  # merge list
+  res <- Reduce(function(...) merge(..., by = keys, all = TRUE), ls)
+  
+  # format result
+  row.names(res) <- res[, keys[1]]
+  res[, keys[1]] <- NULL
+  return(res)
 }
 
 
@@ -246,10 +273,10 @@ merge_dataframes <- function(ls, keys = "SYMBOL") {
 #    statistics.
 
 f.Q <- function (dadj, varadj) {
-    w <- 1/varadj
-    tmp1 <- w * dadj
-    mu <- rowSums(tmp1, na.rm = TRUE)/rowSums(w, na.rm = TRUE)
-    Q <- rowSums(w * (dadj - mu)^2, na.rm = TRUE)
+  w <- 1/varadj
+  tmp1 <- w * dadj
+  mu <- rowSums(tmp1, na.rm = TRUE)/rowSums(w, na.rm = TRUE)
+  Q <- rowSums(w * (dadj - mu)^2, na.rm = TRUE)
 }
 
 
@@ -267,10 +294,10 @@ f.Q <- function (dadj, varadj) {
 # @return A vector of tau values.
 
 tau2.DL <- function (Q, num.studies, my.weights) {
-    tmp1 <- rowSums(my.weights, na.rm = TRUE)
-    tmp2 <- rowSums(my.weights^2, na.rm = TRUE)
-    value <- cbind((Q - (num.studies - 1))/(tmp1 - (tmp2/tmp1)), 0)
-    apply(value, 1, max)
+  tmp1 <- rowSums(my.weights, na.rm = TRUE)
+  tmp2 <- rowSums(my.weights^2, na.rm = TRUE)
+  value <- cbind((Q - (num.studies - 1))/(tmp1 - (tmp2/tmp1)), 0)
+  apply(value, 1, max)
 }
 
 
@@ -287,9 +314,9 @@ tau2.DL <- function (Q, num.studies, my.weights) {
 # @return A vector with the estimates of the overall mean effect sizes.
 
 mu.tau2 <- function (my.d, my.vars.new) {
-    w <- 1/my.vars.new
-    tmp1 <- w * my.d
-    mu <- rowSums(tmp1, na.rm = TRUE)/rowSums(w, na.rm = TRUE)
+  w <- 1/my.vars.new
+  tmp1 <- w * my.d
+  mu <- rowSums(tmp1, na.rm = TRUE)/rowSums(w, na.rm = TRUE)
 }
 
 
@@ -303,6 +330,6 @@ mu.tau2 <- function (my.d, my.vars.new) {
 # @return
 
 var.tau2 <- function (my.vars.new) {
-    w <- 1/my.vars.new
-    my.var <- 1/rowSums(w, na.rm = TRUE)
+  w <- 1/my.vars.new
+  my.var <- 1/rowSums(w, na.rm = TRUE)
 }
